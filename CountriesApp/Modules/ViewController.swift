3771bd19cf2059428eb0ffe8c1f4e64dd4a109fe
Luis Fernando Sánchez Palma <luisfer.san.palm@gmail.com>
2024-05-08 18:26:00 -0600

    [UPDATE]Se inicia proyecto CountriesApp

---
+++ CountriesApp/Modules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountriesApp
+//
+//  Created by Luis Fernando Sánchez Palma on 08/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
